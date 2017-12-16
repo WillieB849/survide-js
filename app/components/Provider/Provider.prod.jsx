7dec8bac5b103ba4f872896536ce20f667490fc8
William Benitez <williammiguel09@gmail.com>
2017-12-16 16:30:52 +0000

    first commit

---
+++ app/components/Provider/Provider.prod.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AltContainer from 'alt-container';
+import alt from '../../libs/alt';
+import setup from './setup';
+
+setup(alt);
+
+function provider(props) {
+  const children = props.children;
+  
+return (
+  <AltContainer flux={alt}>
+    {children}
+  </AltContainer>
+  );
+};
+
+export default provider;
--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,9 @@ switch(TARGET) {
       parts.setupCSS(PATHS.style),
       parts.devServer({
         // Customize host/port here if needed
-        host: process.env.HOST,
+        //host: process.env.HOST,
+        host: 'reactclass2-willieb849.c9users.io',
+        https: false,
         port: process.env.PORT,
         poll: ENABLE_POLLING
       }),
